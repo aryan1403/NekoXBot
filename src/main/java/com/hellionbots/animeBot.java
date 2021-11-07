5588a1cc0fbe9bb468abc8ca7b76a9d2bc7d825f
Aaryan <aaryan14032006@gmail.com>
2021-11-07 15:11:51 +0530

    Updated

--- src/main/java/com/hellionbots/animeBot.java
+++ src/main/java/com/hellionbots/animeBot.java
@@ -1,7 +1,12 @@
 package com.hellionbots;
 
+import com.hellionbots.Services.cfg;
+
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 public class animeBot extends TelegramLongPollingBot {
 
@@ -10,14 +15,33 @@ public class animeBot extends TelegramLongPollingBot {
         
     }
 
+    public Message sendMessage(Update update, String text) {
+        Message m;
+
+        try {
+            SendMessage message = new SendMessage(update.getMessage().getChatId().toString(), text);
+            // message.enableMarkdownV2(true);
+            m = execute(message);
+            return m;
+        } catch (TelegramApiException e) {
+            System.out.println(e.getMessage());
+        }
+
+        return null;
+    }
+
+    public String handler() {
+        return cfg.handler;
+    }
+
     @Override
     public String getBotUsername() {
-        return "";
+        return cfg.botUsername;
     }
 
     @Override
     public String getBotToken() {
-        return "";
+        return cfg.botToken;
     }
     
 }
