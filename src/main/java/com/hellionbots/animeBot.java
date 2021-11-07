a02729537987e87385ba16f755188dd94e749199
Aaryan <aaryan14032006@gmail.com>
2021-11-07 15:06:49 +0530

    Created bot

---
+++ src/main/java/com/hellionbots/animeBot.java
@@ -0,0 +1,23 @@
+package com.hellionbots;
+
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+public class animeBot extends TelegramLongPollingBot {
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        
+    }
+
+    @Override
+    public String getBotUsername() {
+        return "";
+    }
+
+    @Override
+    public String getBotToken() {
+        return "";
+    }
+    
+}
