edfcc60ad20ad35310b4287db9c05d0f0584e261
Aaryan <aaryan14032006@gmail.com>
2021-11-07 15:56:01 +0530

    App to final

--- src/main/java/com/hellionbots/App.java
+++ src/main/java/com/hellionbots/App.java
@@ -1,10 +1,10 @@
 package com.hellionbots;
 
 import org.telegram.telegrambots.meta.TelegramBotsApi;
-import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
-public class App 
+public final class App 
 {
     public static void main(String[] args) {
         try {
